c376af5d35f715df4780644bc5a8fe27f63bdc12
Yousuf897 <ahmad.samandar@tekschool.us>
2024-07-15 20:11:09 -0400

    Activity1 week 2

--- src/test/java/tek/locators/other/Activity1.java
+++ src/test/java/tek/locators/other/Activity1.java
@@ -1,5 +1,10 @@
 package tek.locators.other;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Activity1 {
 
     /*
@@ -8,4 +13,31 @@ public class Activity1 {
     use name, id, linkText or anything that is available.
     15 Minute.
      */
+    public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://retail.tekschool-students.com/");
+        driver.manage().window().maximize();
+
+        By signInLocator = By.linkText("Sign in");
+        WebElement signInElement = driver.findElement(signInLocator);
+        signInElement.click();
+
+        driver.findElement(By.linkText("Create New Account")).click();
+
+        driver.findElement(By.name("name")).sendKeys("Mohammad");
+
+        driver.findElement(By.name("email")).sendKeys("mohammad@123123@gmail.com");
+
+        driver.findElement(By.name("password")).sendKeys("Password@123");
+
+        driver.findElement(By.name("confirmPassword")).sendKeys("Password@123");
+
+        driver.findElement(By.id("signupBtn")).click();
+
+        String errorText = driver.findElement(By.className("error")).getText();
+
+        System.out.println(errorText);
+
+        driver.quit();
+    }
 }
