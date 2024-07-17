a1b3c0985e13ee324d12670b57c7d82b70884c95
Yousuf897 <ahmad.samandar@tekschool.us>
2024-07-16 20:55:24 -0400

    absolute xpath

---
+++ src/test/java/tek/locators/xpath/Activity1.java
@@ -0,0 +1,10 @@
+package tek.locators.xpath;
+
+public class Activity1 {
+    /*
+    In retail App click on Videos Games then choose PS5 console
+    then click on add to cart
+    Only use relative xpath.
+    Push to your GitHUB repository
+     */
+}
