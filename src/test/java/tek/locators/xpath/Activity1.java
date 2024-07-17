f25c5d59541578625a99db91ed81eb00c7ece82e
Yousuf897 <ahmad.samandar@tekschool.us>
2024-07-16 21:39:47 -0400

    xpath activity

--- src/test/java/tek/locators/xpath/Activity1.java
+++ src/test/java/tek/locators/xpath/Activity1.java
@@ -1,5 +1,9 @@
 package tek.locators.xpath;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Activity1 {
     /*
     In retail App click on Videos Games then choose PS5 console
@@ -7,4 +11,28 @@ public class Activity1 {
     Only use relative xpath.
     Push to your GitHUB repository
      */
+    public static void main(String[] args) throws InterruptedException {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://retail.tekschool-students.com/");
+        driver.manage().window().maximize();
+
+        Thread.sleep(1000);
+
+        driver.findElement(By.xpath("//img[@alt='Video Games']"))
+                .click();
+
+        Thread.sleep(1000);
+        // you can use contain with Text as well on Xpath Example: `//p[contains(text(), 'Console')]`
+        driver.findElement(By.xpath("//p[text()='PlayStation 5 Console']"))
+                .click();
+
+        Thread.sleep(1000);
+
+        String text = driver.findElement(By.xpath("//h1[@class='product__name']")).getText();
+        System.out.println(text);
+
+        driver.findElement(By.xpath("//button[@id='addToCartBtn']")).click();
+
+
+    }
 }
